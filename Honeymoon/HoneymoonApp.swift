5093e9951e18d601fe8533056c97c531ad66561e
Mohd Rozaini <zainirejab@gmail.com>
2021-11-03 11:09:56 +0200

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Bismillah on 3.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
